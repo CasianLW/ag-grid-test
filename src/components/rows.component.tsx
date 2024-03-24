135cc9cf188cd372f61e303e5efdf0d8d989e7fd
CasianLW <ciorbacasian@gmail.com>
2024-03-25 00:53:05 +0100

    rows tests inspiration by docs

---
+++ src/components/rows.component.tsx
@@ -0,0 +1,74 @@
+import React, { FC, useMemo, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+
+import { ColDef, ICellRendererParams } from "ag-grid-community";
+
+interface RowData {
+  time: string;
+  session: string;
+  speaker: string;
+  span: number;
+}
+const InitialRowData: RowData[] = [
+  { time: "09:00", session: "Keynote", speaker: "Dr. Smith", span: 3 },
+  { time: "10:00", session: "", speaker: "", span: 0 },
+  { time: "10:30", session: "", speaker: "", span: 0 },
+  { time: "11:00", session: "React Workshop", speaker: "Jane Doe", span: 1 },
+  { time: "12:00", session: "Lunch Break", speaker: "", span: 1 },
+  { time: "14:00", session: "Presentation", speaker: "Dr.Smith", span: 3 },
+  { time: "14:30", session: "", speaker: "", span: 0 },
+  { time: "15:00", session: "", speaker: "", span: 0 },
+  { time: "15:30", session: "Break", speaker: "", span: 1 },
+  { time: "16:30", session: "Monologue", speaker: "Dr.Vincent", span: 1 },
+];
+const rowSpan = (params: { data: RowData }) => params.data.span;
+const cellStyle = (params: { data: RowData }) => {
+  if (params.data.span > 1) {
+    return {
+      backgroundColor: "rgba(255, 255, 255, .4)",
+      color: "rgba(255, 255, 255, .7)",
+    }; // Light gray background for spanned cells
+  } else {
+    return null; // No specific style for other cells
+  }
+};
+const SessionCellRenderer: FC<ICellRendererParams> = ({ value, data }) => {
+  if (data.span === 0) return null; // Don't render content for spanned rows
+  return <>{value}</>; // Render the value for non-spanned rows
+};
+
+export const RowsGrid: FC = () => {
+  const [rowData] = useState<RowData[]>(InitialRowData);
+
+  const columnDefs = useMemo<ColDef[]>(
+    () => [
+      { field: "time" },
+      {
+        field: "session",
+        cellRendererFramework: SessionCellRenderer,
+        rowSpan: rowSpan,
+        cellStyle: cellStyle,
+      },
+      { field: "speaker" },
+    ],
+    []
+  );
+
+  return (
+    <div
+      className="ag-theme-quartz-dark m-auto"
+      style={{ height: 400, width: 1000 }}
+    >
+      <h2 className="text-black text-2xl mt-12 font-semibold">Rows tests</h2>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        domLayout="autoHeight"
+        getRowHeight={() => 60}
+        suppressRowTransform={true}
+      />
+    </div>
+  );
+};
+
+export default RowsGrid;
