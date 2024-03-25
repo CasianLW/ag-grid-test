9bd7431acacad476cff978951c6cdaa7f9d7b810
CasianLW <ciorbacasian@gmail.com>
2024-03-25 03:12:32 +0100

    added edit grid to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ import "@ag-grid-community/styles/ag-theme-quartz.css";
 import { ColGrid } from "./components/colls.component";
 import { RowsGrid } from "./components/rows.component";
 import CellsGrid from "./components/cells.component";
+import EditGrid from "./components/edit.component";
 
 function App() {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-1 pb-40">
       <ColGrid />
       <div className="block md:flex max-w-[1000px] gap-4 mx-auto">
         <RowsGrid />
         <CellsGrid />
       </div>
+      <EditGrid />
     </div>
   );
 }
